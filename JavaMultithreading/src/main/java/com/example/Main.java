38d6ad6d2ccba4b1b985853c44acb9425da8ceda
DavidJeonKr <dudgh7410@naver.com>
2022-08-15 22:34:25 +0900

    MultiThread bank

---
+++ JavaMultithreading/src/main/java/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                throw new RuntimeException("Intentinal Exception");
+
+            }
+        });
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happend in thread " + t.getName()
+                    + " the error is " + e.getMessage());
+            }
+        });
+        thread.start();
+    }
+}
